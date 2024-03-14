f4a5a4c828ea94ee3fa088b68b76bb7dc2a99500
d-kens <omondikens1@gmail.com>
2024-03-14 09:34:23 +0300

    Applying validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,14 @@
-// DTO for the data we will be receiving in the request
+import { IsEmail, IsEnum, IsNotEmpty, IsString} from "class-validator";
 export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @IsEnum(["INTERN", "ENGINEER", "ADMIN"], {
+        message: 'Valid role required'
+    })
     role: "INTERN" | "ENGINEER" | "ADMIN";
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, ParseIntPipe, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUSerDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController { 
@@ -18,17 +20,16 @@ export class UsersController {
     }
 
     @Post() // POST /users
-    create(@Body() user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) 
+    create(@Body(ValidationPipe) createUseDto: CreateUserDto) 
     {
-        return this.usersService.create(user)
+        return this.usersService.create(createUseDto)
     }
 
     
-
     @Patch(':id') // PATCH /users/:id
-    update(@Param('id', ParseIntPipe) id: number, @Body() userUpdate: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }) 
+    update(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateUserDto: UpdateUSerDto) 
     {
-        return this.usersService.update(id, userUpdate);
+        return this.usersService.update(id, updateUserDto);
     }
 
     @Delete(':id') // DELETE /users/:id
