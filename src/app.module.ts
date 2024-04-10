08e0b0d672cfaeb95a929190fd8082675a7a3a0f
d-kens <omondikens1@gmail.com>
2024-04-10 10:19:00 +0300

    module and constrollers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-
+import { CatsController } from './cats/cats.controller';
+import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CatsModule],
 })
 export class AppModule {}
